f388b920c052bcdcc8e3464a3bfeeca0def2750c
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-07-24 21:47:14 +0300

    Add files via upload

---
+++ app/src/test/java/ru/tutu/github_actions_sample/ExampleUnitTest.kt
@@ -0,0 +1,24 @@
+package ru.tutu.github_actions_sample
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
+
+//class ExampleFailTest {
+//    @Test
+//    fun addition_isCorrect() {
+//        assertEquals(5, 2 + 2)
+//    }
+//}
